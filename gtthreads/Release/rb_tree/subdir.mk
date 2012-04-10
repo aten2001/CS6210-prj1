18ece3e0065ed513cd197409afa07e428d932bf2
Sam Britt <samuelbritt@gmail.com>
2012-04-10 17:53:46 -0400

    move rb tree tests to its own directory

--- gtthreads/Release/rb_tree/subdir.mk
+++ gtthreads/Release/rb_tree/subdir.mk
@@ -7,19 +7,16 @@ C_SRCS += \
 ../rb_tree/misc.c \
 ../rb_tree/red_black_tree.c \
 ../rb_tree/stack.c \
-../rb_tree/test_red_black_tree.c 
 
 OBJS += \
 ./rb_tree/misc.o \
 ./rb_tree/red_black_tree.o \
 ./rb_tree/stack.o \
-./rb_tree/test_red_black_tree.o 
 
 C_DEPS += \
 ./rb_tree/misc.d \
 ./rb_tree/red_black_tree.d \
 ./rb_tree/stack.d \
-./rb_tree/test_red_black_tree.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
