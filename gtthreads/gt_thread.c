11094eeb5e3f1deb61f8958cb1edd26039a183e0
Sam Britt <samuelbritt@gmail.com>
2012-04-15 21:57:10 -0400

    cleaner kthread exit notification

--- gtthreads/gt_thread.c
+++ gtthreads/gt_thread.c
@@ -32,21 +32,8 @@ gt_spinlock_t MALLOC_LOCK = GT_SPINLOCK_INITIALIZER;
 int kthread_count = 0;
 gt_spinlock_t kthread_count_lock = GT_SPINLOCK_INITIALIZER;
 
-/* Linked list for keeping track of the child kthreads, so we can signal them
- * to exit */
-typedef struct kthread_node {
-	kthread_t *k_ctx;
-	struct kthread_node *next;
-} kthread_node_t;
-
-kthread_node_t *kthread_list = NULL;
-
-static void append_kthread(kthread_t *k_ctx) {
-	kthread_node_t *node = emalloc(sizeof(*node));
-	node->k_ctx = k_ctx;
-	node->next = kthread_list;
-	kthread_list = node;
-}
+/* Global used to signal to the kthreads that they can exit when ready */
+extern int can_exit;
 
 void gtthread_options_init(gtthread_options_t *options)
 {
@@ -87,7 +74,6 @@ static void _gtthread_app_init(gtthread_options_t *options)
 	for (int lwp = 0; lwp < options->lwp_count; lwp++) {
 		if (!(k_thread = kthread_create(&k_tid, lwp)))
 			fail_perror("kthread_create");
-		append_kthread(k_thread);
 		gt_spin_lock(&kthread_count_lock);
 		kthread_count++;
 		gt_spin_unlock(&kthread_count_lock);
@@ -103,24 +89,19 @@ void wakeup(int signo)
 void gtthread_app_exit()
 {
 	checkpoint("%s", "Entering app_exit");
-	/* first we signal to the kthreads that it is OK to exit. At this point,
-	 * they shouldn't need to wait for any more uthreads to be created */
-	kthread_node_t *p = kthread_list;
-	while (p != NULL) {
-		checkpoint("k%d (%d): Sending exit signal", p->k_ctx->cpuid,
-			   p->k_ctx->pid);
-		kill(p->k_ctx->pid, SIGUSR1);
-		p = p->next;
-	}
 
 	/* be sure to wake from sleep */
 	sig_install_handler_and_unblock(SIGCHLD, wakeup);
+
+	/* first we signal to the kthreads that it is OK to exit. At this point,
+	 * they shouldn't need to wait for any more uthreads to be created */
+	can_exit = 1;
 
 	gt_spin_lock(&kthread_count_lock);
 	while (kthread_count) {
 		checkpoint("%s", "Waiting for children");
 		gt_spin_unlock(&kthread_count_lock);
-		sleep(5);
+		sleep(1);
 		gt_spin_lock(&kthread_count_lock);
 	}
 	gt_spin_unlock(&kthread_count_lock);
